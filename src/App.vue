f090d930e843b8123480e9ac9cad752d258cf3ca
Shashwat Tripathi <stripathix@gmail.com>
2018-12-27 14:16:44 +0530

    Menu integrated

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,46 @@
 <template>
   <div id="app">
+    <div>
+   <md-menu md-direction="bottom-start">
+     <md-button md-menu-trigger>Bottom Start</md-button>
+
+     <md-menu-content>
+       <md-menu-item><router-link to="/">Home</router-link></md-menu-item>
+       <md-menu-item>My Item 2</md-menu-item>
+       <md-menu-item>My Item 3</md-menu-item>
+     </md-menu-content>
+   </md-menu>
+
+   <md-menu md-direction="bottom-end">
+     <md-button md-menu-trigger>Bottom End</md-button>
+
+     <md-menu-content>
+       <md-menu-item>My Item 1</md-menu-item>
+       <md-menu-item>My Item 2</md-menu-item>
+       <md-menu-item>My Item 3</md-menu-item>
+     </md-menu-content>
+   </md-menu>
+
+   <md-menu md-direction="top-start">
+     <md-button md-menu-trigger>Top Start</md-button>
+
+     <md-menu-content>
+       <md-menu-item>My Item 1</md-menu-item>
+       <md-menu-item>My Item 2</md-menu-item>
+       <md-menu-item>My Item 3</md-menu-item>
+     </md-menu-content>
+   </md-menu>
+
+   <md-menu md-direction="top-end">
+     <md-button md-menu-trigger>Top End</md-button>
+
+     <md-menu-content>
+       <md-menu-item>My Item 1</md-menu-item>
+       <md-menu-item>My Item 2</md-menu-item>
+       <md-menu-item>My Item 3</md-menu-item>
+     </md-menu-content>
+   </md-menu>
+ </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
